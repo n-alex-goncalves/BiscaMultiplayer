a4837e24745959d51455651874d0e316d4f34385
NunoAGoncalves <goncalvesnuno01@gmail.com>
2023-05-01 18:58:59 +0100

    Changed HTTP to HTTTPS again to prevent a mixed content error during deployment

--- src/socket.js
+++ src/socket.js
@@ -2,8 +2,8 @@ import io from 'socket.io-client';
 
 const hostname = window.location.hostname;
 const port = process.env.PORT || 8000;
-const socket = io.connect(`http://${hostname}:${port}`);
+const socket = io.connect(`https://${hostname}:${port}`);
 
-console.log(`http://${hostname}:${port}`);
+console.log(`https://${hostname}:${port}`);
 
 export default socket;
