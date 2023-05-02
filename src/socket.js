9e3dcb60ed5ef9a44d5c328c928bbed678f448d4
NunoAGoncalves <goncalvesnuno01@gmail.com>
2023-05-02 14:28:04 +0100

    Removed the port number from the socket.io client to see if that resolves deployment issues

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,7 @@ import io from 'socket.io-client';
 
 const hostname = window.location.hostname;
 const port = process.env.PORT || 8000;
-const socket = io.connect(`https://${hostname}:${port}`);
+const socket = io.connect(`https://${hostname}`);
+console.log('socket: ', `https://${hostname}:${port}`);
 
 export default socket;
