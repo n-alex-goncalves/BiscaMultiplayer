0a263397b30b887d913f7707e7ba47325affdaf6
NunoAGoncalves <goncalvesnuno01@gmail.com>
2023-06-23 17:51:21 +0100

    Added license text to the repository

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 const hostname = window.location.hostname;
 const port = process.env.PORT || 8000;
 
-const socket = io.connect(`http://${hostname}:${port}`);
-//const socket = io.connect(`https://${hostname}`);
+//const socket = io.connect(`http://${hostname}:${port}`);
+const socket = io.connect(`https://${hostname}`);
 
 export default socket;
